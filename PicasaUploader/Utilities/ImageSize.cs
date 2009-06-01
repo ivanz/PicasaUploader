936d13e480fd1690b0026262917bd2538384244e
Ivan N. Zlatev <contact@i-nz.net>
2009-06-01 18:51:22 +0100

    Add some license headers.

--- PicasaUploader/Utilities/ImageSize.cs
+++ PicasaUploader/Utilities/ImageSize.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿//
+// Copyright (c) 2009 Ivan N. Zlatev <contact@i-nz.net>
+//
+// Authors:
+//	Ivan N. Zlatev <contact@i-nz.net>
+//
+// License: MIT/X11 - See LICENSE.txt
+//
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
